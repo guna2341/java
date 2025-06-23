b0d3785df56f1563d003be403b8ecb45846bd006
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-06-23 16:14:49 +0530

    Update squareNumberpattern.java

--- highend/squareNumberpattern.java
+++ highend/squareNumberpattern.java
@@ -5,17 +5,16 @@ public class Main
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		for(int i=0;i<n-1;i++) {
+		for(int i=0;i<n;i++) {
 		    
 		    int count = n;
 		    int t;
-		    if (i >= (int) (n/2)+1) {
-		        t = n-i-2;
+		    if (i >= (int) (n/2)-1) {
+		        t = n-i-1;
 		    }
 		    else {
 		        t = i;
 		    }
-		    System.out.print(t + " ");
 		    for(int j=0;j<n;j++) {
 		        if (j < t) {
 		            System.out.print(count);
