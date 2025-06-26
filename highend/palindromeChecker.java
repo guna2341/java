d77a8ec87095c113310c5793c1a5cc86e615839e
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-06-26 13:53:59 +0530

    Create palindromeChecker.java

---
+++ highend/palindromeChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int temp = n;
+        int rev = 0;
+        while (n > 0) {
+          rev = (rev*10) + (n%10);
+          n /= 10;
+      }
+        System.out.print(rev == temp);
+    }
+}
